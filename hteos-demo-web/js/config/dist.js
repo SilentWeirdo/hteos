d6ad4fee3fab5e5151fcf664a9beb166dcbc6e13
leecho571 <leecho571@vip.qq.com>
2018-08-10 09:06:11 +0800

    优化文档和开发环境配置

--- hteos-demo-web/js/config/dist.js
+++ hteos-demo-web/js/config/dist.js
@@ -2,7 +2,6 @@
     HteOS.debug = false;
     HteOS.server = "http://demo.app.hteos.com";
     HteOS.wsServer= "ws://demo.app.hteos.com/ws/messager";
-    //HteOS.server = "http://localhost:8080";
     HteOS.basePath = "http://asset.hteos.com/hteos/4.0/";
 
 })();
