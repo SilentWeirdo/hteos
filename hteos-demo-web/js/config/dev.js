69832341028e683e3dce3cf87c9a7728de689a1b
Administrator <wxw@gfire.cn>
2018-10-25 10:43:08 +0800

    修正请求端口

--- hteos-demo-web/js/config/dev.js
+++ hteos-demo-web/js/config/dev.js
@@ -1,5 +1,5 @@
 (function () {
     HteOS.debug = true;
-    HteOS.server = "http://localhost";
+    HteOS.server = "http://localhost:8080";
     HteOS.basePath = "http://asset.hteos.com/hteos/4.0/";
 })();
